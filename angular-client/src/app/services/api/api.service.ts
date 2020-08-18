348b466e3ece40de9c2360a4cda7af3aaf3615ba
David Wickizer <dwickizer@geocent.com>
2020-08-18 00:14:05 -0400

    beginning to write base HTTP Client Service

--- angular-client/src/app/services/api/api.service.ts
+++ angular-client/src/app/services/api/api.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
-import { map } from 'rxjs/operators';
-import { Observable, of, from } from 'rxjs';
+import { map, catchError, retry } from 'rxjs/operators';
+import { Observable, of, from, throwError } from 'rxjs';
 
+import { User } from '../../models/user/user';
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +23,42 @@ export class ApiService {
   // Get all users from the API
   getAllPeople(): Observable<any> {
     return this.http.get(`${this.API}/users`);
+  }
+
+  add(): Observable<any> {
+    return null;
+  }
+
+  get(id: string): Observable<User> {
+    return this.http.get<User>(`${this.API}/users/${id}`)
+    .pipe(catchError(this.handleError));
+  }
+
+  update(): Observable<any> {
+    return null;
+  }
+
+  delete(): Observable<any> {
+    return null;
+  }
+
+  getAll(): Observable<any> {
+    return null;
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
