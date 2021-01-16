f22769431c321ada84d2825f968c50ea7f9266f0
David Wickizer <dawickizer1@gmail.com>
2021-01-16 14:56:51 -0500

    Made a playground mode for practicing babylon

--- angular-client/src/app/app-routing.module.ts
+++ angular-client/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { TestPhaserComponent } from './components/test-phaser/test-phaser.compon
 import { TestBabylonComponent } from './components/test-babylon/test-babylon.component';
 import { TestApiComponent } from './components/test-api/test-api.component';
 import { TestSocketComponent } from './components/test-socket/test-socket.component';
+import { PlaygroundComponent } from './components/playground/playground.component';
 
 const routes: Routes = [
   { path: 'test-phaser', component: TestPhaserComponent },
   { path: 'test-babylon', component: TestBabylonComponent },
   { path: 'test-api', component: TestApiComponent },
-  { path: 'test-socket', component: TestSocketComponent }
+  { path: 'test-socket', component: TestSocketComponent },
+  { path: 'playground', component: PlaygroundComponent }
 ];
 
 @NgModule({
