71b5122fe086cecb7a930b58819c634cdbdcd6c3
David Wickizer <dawickizer1@gmail.com>
2021-01-23 13:16:56 -0500

    Neatened up the Dockerfile.prods. Changed the environment production endpoint in angular-client to an AWS ELB. Will change later

--- angular-client/src/environments/environment.prod.ts
+++ angular-client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  EXPRESS_ENDPOINT: 'http://localhost:3000'
+  EXPRESS_ENDPOINT: 'http://aa218f489564649379461e744107e739-1995998504.us-east-1.elb.amazonaws.com'
 };
