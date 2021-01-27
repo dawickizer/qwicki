7f498b3730073e69368b6b198aebb129e5393b21
David Wickizer <dawickizer1@gmail.com>
2021-01-27 13:33:09 -0500

    Figured out how to persist data in mongo db by using docker volumes. Figured out how to run an init script on mongo if there is no data present in db/data. Figured out how to create a user that is attached to the mean db for authentication

--- express-server/server/config/config.ts
+++ express-server/server/config/config.ts
@@ -1,9 +1,11 @@
 const config: any = {
     development: {
-        db: 'mongodb://database/mean' // MongoDB URL from the docker-compose file
+        db: 'mongodb://username:password@database/mean' // MongoDB URL from the docker-compose file
     },
     production: {
         db: process.env.MONGO_DB_ENDPOINT
     }
 };
 export default config;
+
+// 'mongodb://' + process.env.MONGO_DB_USERNAME + ':' + process.env.MONGO_DB_PASSWORD + '@' + process.env.MONGO_DB_SERVICE + '/' + process.env.MONGO_DB_NAME
