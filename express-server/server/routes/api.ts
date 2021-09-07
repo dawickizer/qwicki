2735de1229a51bbe8f600e8a38c860c47a7dc2ef
David Wickizer <dawickizer1@gmail.com>
2021-09-07 03:35:01 -0400

    Dockerized colyseus-chat. Proof of concept for communicating between express and colyseus

--- express-server/server/routes/api.ts
+++ express-server/server/routes/api.ts
@@ -6,6 +6,12 @@ const router = Router();
 // GET api listing.
 router.get('/', (_req, res) => {
     res.send('api works with nodemon!!');
+
+    http.get('http://colyseus-chat:2567', (resp) => {
+
+    }).on("error", (err) => {
+    console.log("Error: " + err.message);
+    });
 });
 
 export default router;
