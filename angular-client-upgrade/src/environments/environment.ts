1340e45d389881828c9ddc0e416ac601fa195de6
David Wickizer <dawickizer1@gmail.com>
2022-11-09 16:37:08 -0500

    Added root level files and some src files. App still builds/runs

--- angular-client-upgrade/src/environments/environment.ts
+++ angular-client-upgrade/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  EXPRESS_SERVER: 'http://localhost:3000',
+  COLYSEUS_CHAT: 'ws://localhost:2567'
 };
 
 /*
