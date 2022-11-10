603920f7862550d07ee3e9cf176923d532c10ed4
David Wickizer <dawickizer1@gmail.com>
2022-11-09 20:39:11 -0500

    App builds but console errors

--- angular-client-upgrade/src/app/app-routing.module.ts
+++ angular-client-upgrade/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { BabylonjsComponent } from './components/babylonjs/babylonjs.component';
+import { AuthGuardService } from './services/auth/auth.service';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, canActivate: [AuthGuardService] },
+  { 
+    path: 'auth',
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'signup', component: SignupComponent }
+    ]
+  },
+  { path: 'babylonjs', component: BabylonjsComponent, canActivate: [AuthGuardService] },
+  { path: 'user-profile', component: UserProfileComponent, canActivate: [AuthGuardService] },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- angular-client-upgrade/src/app/app.component.ts
+++ angular-client-upgrade/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from './services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-client-upgrade';
+export class AppComponent implements OnInit {
+  title = 'Qwicki';
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  ngOnInit(): void {}
 }
