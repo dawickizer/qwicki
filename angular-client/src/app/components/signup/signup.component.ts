ae993de94004ddfa5151dec9783614c3bf9bed49
David Wickizer <dawickizer1@gmail.com>
2023-09-05 15:05:59 +0900

    Simplified Signup component

--- angular-client/src/app/components/signup/signup.component.ts
+++ angular-client/src/app/components/signup/signup.component.ts
@@ -1,28 +1,17 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Credentials } from 'src/app/models/credentials/credentials';
+import { Component } from '@angular/core';
 import { User } from 'src/app/models/user/user';
 import { Store } from '@ngrx/store';
 import { signup } from 'src/app/state/user/user.actions';
-import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent implements OnInit, OnDestroy {
-  credentials: Credentials = new Credentials();
+export class SignupComponent {
   user: User = new User();
-  unsubscribe$ = new Subject<void>();
 
   constructor(private store: Store) {}
-
-  ngOnInit(): void {}
-
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
-  }
 
   signup() {
     this.store.dispatch(signup({ user: new User(this.user), route: '/' }));
