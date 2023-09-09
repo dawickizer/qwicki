fc8a6a2ef6cf98f3ceae86122fc09adb6efc3a86
David Wickizer <dawickizer1@gmail.com>
2023-09-10 03:47:59 +0900

    Return just id on online/offline events

--- colyseus-social/src/managers/PresenceManager.ts
+++ colyseus-social/src/managers/PresenceManager.ts
@@ -8,11 +8,11 @@ export class PresenceManager extends SocialManager {
   }
 
   notifyHostUserIsOnline(user: User) {
-    this.socialRoom.hostClient.send('online', user);
+    this.socialRoom.hostClient.send('online', user._id);
   }
 
   notifyHostUserIsOffline(user: User) {
-    this.socialRoom.hostClient.send('offline', user);
+    this.socialRoom.hostClient.send('offline', user._id);
   }
 
   broadcastDisposeRoom() {
