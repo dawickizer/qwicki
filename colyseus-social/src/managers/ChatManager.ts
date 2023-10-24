9ff7cc3746e750227c114fe1b0ddf8a4f2152b29
David Wickizer <dawickizer1@gmail.com>
2023-10-24 11:10:54 +0900

    Add null check to messageUser listener

--- colyseus-social/src/managers/ChatManager.ts
+++ colyseus-social/src/managers/ChatManager.ts
@@ -14,8 +14,10 @@ export class ChatManager extends InboxManager {
 
     this.inbox.onMessage('messageUser', (client, message) => {
       const user = this.inbox.getUserById(message.to._id);
-      const userClient: Client = this.inbox.getClient(user);
-      userClient.send('messageUser', message);
+      if (user) {
+        const userClient: Client = this.inbox.getClient(user);
+        userClient.send('messageUser', message);
+      }
     });
   }
 }
