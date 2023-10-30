e6307e762942ebd08a0ca0cc565c67ca267c9a5a
David Wickizer <dawickizer1@gmail.com>
2023-10-30 13:37:15 +0900

    Change angular-client-upgrade to angular-client

--- angular-client/src/app/app.component.spec.ts
+++ angular-client/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-client-upgrade'`, () => {
+  it(`should have as title 'angular-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-client-upgrade');
+    expect(app.title).toEqual('angular-client');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'angular-client-upgrade app is running!'
+      'angular-client app is running!'
     );
   });
 });
