8dbb575427b55c50cc6af41008afb29a7450e566
David Wickizer <dawickizer1@gmail.com>
2023-12-21 20:07:54 +0900

    Send friend request from lobby panel working

--- angular-client/src/app/components/match-making/lobby-panel/lobby-panel.component.ts
+++ angular-client/src/app/components/match-making/lobby-panel/lobby-panel.component.ts
@@ -3,8 +3,11 @@ import { AuthService } from 'src/app/state/auth/auth.service';
 import { DecodedJwt } from 'src/app/state/auth/decoded-jwt.model';
 import { LobbyOrchestratorService } from 'src/app/state/lobby/lobby.orchestrator.service';
 import { Member } from 'src/app/state/lobby/member.model';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { LobbyService } from 'src/app/state/lobby/lobby.service';
+import { Friend } from 'src/app/state/friend/friend.model';
+import { FriendService } from 'src/app/state/friend/friend.service';
+import { FriendRequestOrchestratorService } from 'src/app/state/friend-request/friend-request.orchestrator.service';
 
 @Component({
   selector: 'app-lobby-panel',
@@ -13,16 +16,20 @@ import { LobbyService } from 'src/app/state/lobby/lobby.service';
 })
 export class LobbyPanelComponent implements OnInit {
   @Input() member: Member;
+  friends$: Observable<Friend[]>;
   decodedJwt$: Observable<DecodedJwt>;
   host$: Observable<Member>;
 
   constructor(
     private lobbyOrchestratorService: LobbyOrchestratorService,
+    private friendRequestOrchestratorService: FriendRequestOrchestratorService,
+    private friendService: FriendService,
     private authService: AuthService,
     private lobbyService: LobbyService
   ) {}
 
   ngOnInit() {
+    this.friends$ = this.friendService.friends$;
     this.decodedJwt$ = this.authService.decodedJwt$;
     this.host$ = this.lobbyService.host$;
   }
@@ -37,5 +44,17 @@ export class LobbyPanelComponent implements OnInit {
 
   leaveLobby() {
     this.lobbyOrchestratorService.leaveLobby().subscribe();
+  }
+
+  isFriend(member: Member): Observable<boolean> {
+    return this.friends$.pipe(
+      map(friends => friends.some(friend => friend._id === member._id))
+    );
+  }
+
+  sendFriendRequest(member: Member) {
+    this.friendRequestOrchestratorService
+      .sendFriendRequest(member.username)
+      .subscribe();
   }
 }
